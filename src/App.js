6dc3f271ecb4a895da4a9b488edaac8118c9fe93
Winston Meikle <winmeikle@gmail.com>
2020-03-10 01:44:38 -0400

    components fixed

--- src/App.js
+++ src/App.js
@@ -4,7 +4,7 @@ import Hero from './components/Hero';
 import Nav from './components/Nav';
 import About from './components/About';
 import Projects from './components/Projects';
-import Contact from './components/Contactt';
+import Contact from './components/Contact';
 import Skills from './components/Skills';
 import Header from './components/Header';
 import Footer from './components/Footer';
